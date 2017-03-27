26c9ab859ff68d4c3c6259b52e512d238c3b1b84
RvDijk <rub0925@gmail.com>
2017-03-27 16:30:19 +0200

    fix mess

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -1,52 +1,41 @@
 package model;
 
-import java.awt.Color;
 import java.awt.GridLayout;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Random;
 import java.util.Scanner;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import org.graphstream.graph.Edge;
 import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.MultiGraph;
 import org.graphstream.ui.view.Viewer;
-import org.graphstream.ui.view.ViewerListener;
-import org.graphstream.ui.view.ViewerPipe;
-import org.graphstream.util.VerboseSink;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
+import logic.Atom;
 import logic.Formula;
+import logic.Knows;
 
-public class Model extends MultiGraph implements ViewerListener {
+public class Model extends MultiGraph {
 	
 	private int worldCount;
-	private ArrayList<Formula> commonKnowledge;
-	private ArrayList<String> agents;
 
-	private ArrayList<String> messages = new ArrayList<String>();
-	
-	private ArrayList<String> clickedWorlds = new ArrayList<>();
-	private ArrayList<Node> selectedNodes = new ArrayList<>();
+	private ArrayList<String> messages = new ArrayList<>();
 
 	public Model() {
 		super("Arbitrary String #1");
 		this.worldCount = 0;
-		this.commonKnowledge = new ArrayList<Formula>();
-		this.agents = new ArrayList<String>();
 
 		Socket socket;
 		try {
@@ -59,6 +48,8 @@ public class Model extends MultiGraph implements ViewerListener {
 
 			int result = JOptionPane.showOptionDialog(null, panel, "Connect to game", JOptionPane.WARNING_MESSAGE, 0,
 					null, buttons, buttons[0]);
+
+			System.out.println(result);
 
 			if (result == 0) {
 				socket = IO.socket("https://mas-ek.herokuapp.com/");
@@ -107,51 +98,44 @@ public class Model extends MultiGraph implements ViewerListener {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		for(int i=0;i<8;++i){
-			addNode(getWorldName());
-		}
-		
-		addAtom("w1","ek1");
-		addAtom("w1","ek2");
-		addAtom("w1","ek3");
-		addAtom("w2","ek1");
-		addAtom("w2","ek2");
-		addAtom("w3","ek1");
-		addAtom("w3","ek3");
-		addAtom("w4","ek2");
-		addAtom("w4","ek3");
-		addAtom("w5","ek1");
-		addAtom("w6","ek2");
-		addAtom("w7","ek3");
-		
-		this.agents.add("fuck");
-		this.agents.add("Joost");
 
-		for(int w1=1;w1<=8;++w1){
-			for(int w2=1;w2<=8;++w2){
-				for(String a : agents){
-					addRelation("w"+w1,"w"+w2,a);
-				}
-			}
-		}
-		
-		ArrayList<String> args1 = new ArrayList<String>();
-		ArrayList<String> args2 = new ArrayList<String>();
-		args1.add("Joost");
-		args1.add("Explode");
-		args2.add("fuck");
-		args2.add("Explode");
+		addNode(getWorldName());
+		addNode(getWorldName());
+		addNode(getWorldName());
 
-		ViewerPipe viewPipe = display().newViewerPipe();
-		viewPipe.addViewerListener(this);
-        viewPipe.addSink(new VerboseSink());
-        viewPipe.pump();
-            	
-		
-        while (true) {
-            viewPipe.pump();
-        }
+		Iterator<Node> nodes = getNodeIterator();
+		Random r = new Random();
+		while (nodes.hasNext()) {
+			Node n = nodes.next();
+			if (r.nextBoolean())
+				addAtom(n.getId(), "p");
+			if (r.nextBoolean())
+				addAtom(n.getId(), "q");
+			n.setAttribute("ui.label", n.getAttribute("atoms").toString());
+		}
+
+		addEdge("w1w2", "w1", "w2");
+		addEdge("w1w1", "w1", "w1");
+		addEdge("w1w3", "w1", "w3");
+		addEdge("w3w1", "w3", "w1");
+		addEdge("w2w3", "w2", "w3");
+		addAgent("w1w2", "Henk");
+		addAgent("w1w2", "Joost");
+		addAgent("w2w3", "Henry");
+
+		addAgent("w1w3", "Up");
+		addAgent("w3w1", "Down");
+		addAgent("w1w1", "Henk");
+
+		System.out.println(new Atom("p").evaluate(getNode("w1")));
+		System.out.println(new Atom("p").evaluate(getNode("w2")));
+		System.out.println(new Atom("p").evaluate(getNode("w3")));
+		Formula f = new Knows(new Atom("p"), "Henk");
+		System.out.println(f.evaluate(getNode("w1")));
+
+		System.out.println();
+
+		display();
 	}
 	
 	private String getWorldName(){
@@ -172,49 +156,14 @@ public class Model extends MultiGraph implements ViewerListener {
 		ArrayList<String> atoms = n.getAttribute("atoms");
 		atoms.add(atom);
 	}
-	
-	public void constructFromFile(String s)
-	{
-		try {
-			BufferedReader in = new BufferedReader(new FileReader(s));
-			
-			String line;
-			while((line = in.readLine()) != null)
-			{
-			    System.out.println(line);
-			    String[] args = line.split(" ");
-			    if (args.length == 2)
-			    	addAtom(args[0], args[1]);
-			    else if (args.length == 4 && args[2].equals("B"))
-			    {
-			    	addRelation(args[0], args[1], args[3]);
-			    	addRelation(args[1], args[0], args[3]);
-			    }
-			    else if (args.length == 4 && args[2].equals("D"))
-			    	addRelation(args[0], args[1], args[3]);
-			}
-			in.close();
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 
 	@Override
 	@SuppressWarnings("unchecked")
-	public Edge addEdge(String agent, String idFrom, String idTo) {
-		//Adds an edge between two world, with at least one agent
-		//DIRECT USE NOT RECOMMENDED
-		//This method assumes the edge doesn't already exist, you'll have to check this before
-		//Using addRelation() does this for you.
-		//(but we could not make this method private as it is inherited)
+	public Edge addEdge(String id, String idFrom, String idTo) {
 		Edge e = super.addEdge(idFrom + idTo, idFrom, idTo, true);
 		if (getEdge(idTo + idFrom) != null) {
 			// symmetric relation, do some styling
+			System.out.println(e.getId());
 			e.setAttribute("ui.class", "symmetric");// tag only applies to one
 													// side to separate the
 													// labels
@@ -223,60 +172,14 @@ public class Model extends MultiGraph implements ViewerListener {
 			// reflexive relation, tag it
 			e.setAttribute("ui.class", "reflexive");
 		}
-		ArrayList<String> agents = new ArrayList<String>();
-		agents.add(agent);
-		e.setAttribute("agents", agents);
+		e.setAttribute("agents", new ArrayList<String>());
 		return e;
 	}
 
-	public void addRelation(String idFrom, String idTo, String agent) {
-		//adds a relation for an agent between two worlds
-		//System.out.println("Adding relation " + idTo+idFrom + " for agent " + agent);
-		Edge e = getEdge(idFrom+idTo);
-		if(e == null){
-			//need to add the edge
-			e = addEdge(agent,idFrom,idTo);
-		}else{
-			ArrayList<String> agents = e.getAttribute("agents");
-			agents.add(agent);
-		}
-	}
-	
-	public void removeRelation(String edgeId, String agent){
-		//remove a relation for an agent between two worlds
-		Edge e = getEdge(edgeId);
-		if(e != null){
-			ArrayList<String> agents = e.getAttribute("agents");
-			if(agents.contains(agent)){
-				//System.out.println("Removing relation " + edgeId + " for agent " + agent);
-				agents.remove(agent);
-				if(agents.isEmpty()){
-					removeEdge(edgeId);
-				}
-				return;
-			}
-		}
-		System.err.println("Tried to remove agent " + agent + "on relation " + edgeId + "while that relation wasn't there!");
-	}
-	
-	public void removeRelation(String idFrom, String idTo, String agent){
-		//remove a relation for an agent between two worlds
-		removeRelation(idFrom+idTo,agent);
-	}
-	
-	public boolean hasRelation(String edgeId, String agent){
-		Edge e = getEdge(edgeId);
-		if(e != null){
-			ArrayList<String> agents = e.getAttribute("agents");
-			return agents.contains(agent);
-		}else{
-			return false;
-		}
-		
-	}
-	
-	public boolean hasRelation(String idFrom, String idTo, String agent){
-		return hasRelation(idFrom+idTo,agent);
+	public void addAgent(String edge, String agent) {
+		Edge e = getEdge(edge);
+		ArrayList<String> agents = e.getAttribute("agents");
+		agents.add(agent);
 	}
 
 	public ArrayList<String> getAtoms(String node) {
@@ -302,120 +205,71 @@ public class Model extends MultiGraph implements ViewerListener {
 		addAttribute("ui.stylesheet", stylesheet);
 
 		Iterator<Node> nodes = getNodeIterator();
-	
 		while (nodes.hasNext()) {
 			Node n = nodes.next();
-			n.addAttribute("ui.color", new Color(0, 0, 0));
 			n.setAttribute("ui.label", " " + n.getId() + ": " + n.getAttribute("atoms").toString());
 		}
 		Iterator<Edge> edges = getEdgeIterator();
 		while (edges.hasNext()) {
 			Edge e = edges.next();
-			//e.setAttribute("ui.label", e.getId() + ": " + e.getAttribute("agents").toString());
-			e.setAttribute("ui.label", "");	
+			e.setAttribute("ui.label", e.getAttribute("agents").toString());
 		}
-		
+
 		return super.display();
 	}
 	
-	private void STF(ArrayList<String> args, int card){
-		String player = args.get(0);
-		if(args.size() > card && args.get(card).equals("Explode")){
-			Iterator<Node> nodes = getNodeIterator();
-			while(nodes.hasNext()){
-				Node n1 = nodes.next();
-				ArrayList<String> atoms = n1.getAttribute("atoms");
-				if(!atoms.contains("ek"+card)){
-					//node contradicts the new information
-					HashSet<String> toRemove = new HashSet<String>();
-					Iterator<Edge> edges = n1.getEdgeIterator();
-					while(edges.hasNext()){
-						//search for edges that need to be removed
-						Edge e = edges.next();
-						toRemove.add(e.getId());
-					}
-					//actually remove the edges
-					for(String e : toRemove){
-						if(hasRelation(e,player)){
-							removeRelation(e,player);
-						}
-					}
-				}
-			}
-		}
-	}
-	
 	private void update(String type, ArrayList<String> args){
-		if(type.equals("STF")){
-			STF(args,1);
-			STF(args,2);
-			STF(args,3);
+		switch(type){
+			case "STF":
+				
+				break;
+			case "BS":
+				
+				break;
+			case "NS":
+				
+				break;
+			case "DS":
+				
+				break;
+			case "NP":
+				
+				break;
+			case "SH":
+				
+				break;
+			case "EK":
+				
+				break;
+			case "ATT":
+				
+				break;
+			case "FV":
+				
+				break;
+			case "S1":
+				Node n = addNode(getWorldName());
+				addAtom(n.getId(),"c1");
+				break;
+			case "S3":
+				
+				break;
+			case "AF":
+				
+				break;
+			case "SP":
+				
+				break;
+			case "DC":
+				
+				break;
+			default:
+				
+				break;
 		}
-	}
-	
-	public ArrayList<Formula> getCommonKnowledge() {
-		return commonKnowledge;
 	}
 
 	public static void main(String[] args) {
 		new Model();
-	}
-
-	@Override
-	public void viewClosed(String viewName) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void buttonPushed(String id) {
-		// TODO Auto-generated method stub
-		
-		Iterator<Edge> it = getNode(id).getEachEdge().iterator();
-		
-		if (selectedNodes.contains(getNode(id)))
-		{
-			selectedNodes.remove(getNode(id));
-			getNode(id).removeAttribute("ui.color");
-			getNode(id).addAttribute("ui.color", new Color(0, 0, 0));
-		}
-		else
-		{
-			selectedNodes.add(getNode(id));
-			getNode(id).removeAttribute("ui.color");
-			getNode(id).addAttribute("ui.color", new Color(255, 0, 0));
-		}
-		
-		Iterator<Node> it3 = super.getNodeIterator();
-		
-		while (it3.hasNext())
-		{
-			Node n3 = it3.next();
-			Iterator<Node> it4 = super.getNodeIterator();
-			while (it4.hasNext())
-			{
-				Node n4 = it4.next();
-				if (n3 != null && n4 != null)
-				{
-					System.out.println(n3.getId() + " " + n4.getId());
-					Edge e2 = getEdge(n3.getId() + n4.getId());
-					if (e2 != null && selectedNodes.contains(n3) && selectedNodes.contains(n4))
-					{
-						e2.setAttribute("ui.label", e2.getAttribute("agents").toString());
-					}
-					else if (e2 != null)
-					{
-						e2.setAttribute("ui.label", "");
-					}
-				}
-			}
-		}
-		
-	}
-
-	@Override
-	public void buttonReleased(String id) {
-		// TODO Auto-generated method stub
-		
 	}
 }
